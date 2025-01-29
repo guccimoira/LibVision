1de2af1aafd252bc2d52badbbe273f3b0c2c99e3
Gucci Moira Zambales <105764031+guccimoira@users.noreply.github.com>
2025-01-29 18:12:42 +0800

    Update ExampleInstrumentedTest.java

--- LibVision/app/src/androidTest/java/com/example/libvision/ExampleInstrumentedTest.java
+++ LibVision/app/src/androidTest/java/com/example/libvision/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.libvision;
+package com.example.imagepro;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.libvision", appContext.getPackageName());
+        assertEquals("com.example.imagepro", appContext.getPackageName());
     }
 }
